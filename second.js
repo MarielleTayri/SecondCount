e3a518fa1a7985547208dda4634aacdfbc77fb34
myriam graine <myriam.bdn@gmail.com>
2023-08-27 17:23:39 +0200

    update counter function

--- second.js
+++ second.js
@@ -8,5 +8,29 @@ document.addEventListener("DOMContentLoaded", () => {
     minutes: document.getElementById("minutes"),
     seconds: document.getElementById("seconds")
   };
+  const messageElement = document.getElementById("message");
 
+  const updateCounter = () => {
+    const currentDate = new Date();
+    const timeDifference = Math.floor((currentDate - targetDate) / 1000);
+
+    const months = Math.floor(timeDifference / (30 * 24 * 60 * 60));
+    const weeks = Math.floor((timeDifference % (30 * 24 * 60 * 60)) / (7 * 24 * 60 * 60));
+    const days = Math.floor((timeDifference % (7 * 24 * 60 * 60)) / (24 * 60 * 60));
+    const hours = Math.floor((timeDifference % (24 * 60 * 60)) / (60 * 60));
+    const minutes = Math.floor((timeDifference % (60 * 60)) / 60);
+    const seconds = timeDifference % 60;
+
+    timeElements.months.textContent = months + " Months";
+    timeElements.weeks.textContent = weeks + " Weeks";
+    timeElements.days.textContent = days + " Days";
+    timeElements.hours.textContent = hours + " Hours";
+    timeElements.minutes.textContent = minutes + " Minutes";
+    timeElements.seconds.textContent = seconds + " Seconds";
+
+    messageElement.textContent = `which makes a total of ${timeDifference} seconds`;
+  };
+
+  updateCounter();
+  setInterval(updateCounter, 1000);
 });
