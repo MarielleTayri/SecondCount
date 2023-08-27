e5ab076f72f55ff2f4632b6ca3d05aa02e2c0226
myriam graine <myriam.bdn@gmail.com>
2023-08-27 17:21:40 +0200

    define main function

---
+++ second.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const targetDate = new Date("2023-04-20T15:55:00");
+  const timeElements = {
+    months: document.getElementById("months"),
+    weeks: document.getElementById("weeks"),
+    days: document.getElementById("days"),
+    hours: document.getElementById("hours"),
+    minutes: document.getElementById("minutes"),
+    seconds: document.getElementById("seconds")
+  };
+
+});
